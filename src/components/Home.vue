db6043c4894cdcc3d154ae16465ae3812e5927d1
rockhopper72 <rockhopper72@users.noreply.github.com>
2019-03-06 14:40:56 -0500

    Add information about myself to homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,9 @@
 <template>
-    <div></div>
+    <div>
+        <h3>Howdy!</h3>
+
+        <p>My name is Steven and I'm a Computer Science student at <a href="https://www.kennesaw.edu/about/">Kennesaw State University</a>. I love crafting websites (frontend and backend), tinkering with a Raspberry Pi, spending time with my little sister and adventuring with my girlfriend.</p>
+    </div>
 </template>
 
 <script></script>
