b2f023b71282c46591016e473092f150aec1cfab
rockhopper72 <github2@steven.globaloffset.com>
2019-03-21 09:12:46 -0400

    Clean layout of left-side content
    Remove the fixed left-side photo and place the right side content to have a max height of view-port and have a scroll bar.
    
    The scroll bar has inertia on iOS.
    
    Fixes #6

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full md:w-1/2 px-2 mt-8">
+    <div class="w-full md:w-1/2 px-2 pt-8 overflow-y-scroll scrolling-touch max-h-screen">
         <h1 class="text-4xl md:text-5xl font-cursive text-center">Steven Kemp</h1>
         <h2 class="font-mono text-lg mt-4 text-center">Web Developer, Student @ KSU, Biking aficionado</h2>
 
