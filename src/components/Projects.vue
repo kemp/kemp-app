b8b5ee75ad32f6325cdfb274fb2ecdef00a32698
rockhopper72 <github2@steven.globaloffset.com>
2019-06-27 19:53:20 -0400

    Add projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,9 +1,53 @@
 <template>
-    <div>
-        <p>Hey there! This section is still a work in progress.</p>
+    <div class="">
+        <div class="mb-4">
+            <h2>Living in Him Alone</h2>
+
+            <p>This blog is a simple example of my expertise with frontend languages such as HTML, CSS, and Javascript (including Vue.js), as well as backend languages such as PHP and Laravel. This website is deployed to the cloud on DigitalOcean, so basic command line experience was necessary to provision the server.</p>
+
+            <p><strong>Keywords:</strong> PHP, Laravel, Javascript, Vue.js</p>
+
+            <p><strong>See it live:</strong> <a href="https://livinginhimalone.com">Living in Him Alone</a></p>
+        </div>
+
+        <div class="mb-4">
+            <h2>Notarizer</h2>
+
+            <p>Notarizer was built with PHP and the Laravel framework. I designed all pages myself using the Sketch Mac app. It has a near-complete test suite and it interacts with Stripe for payments. I’m currently in the process of making a public API for it.</p>
+
+            <p>The source code is available <a href="https://github.com/notarizer/notarizer">on GitHub</a></p>
+
+            <p><strong>Keywords:</strong> PHP, Laravel, SVG</p>
+
+            <p><strong>See it live:</strong> <a href="https://notarizer.app/">Notarizer</a></p>
+        </div>
+
+        <div class="mb-4">
+            <h2>VideoMirror Chrome Extension</h2>
+
+            <p>This is a simple Chrome extension that I made in order to learn about making extensions and practice Javascript. It currently has ≈3,500 active users and 4.9 stars on the Chrome Web Store.</p>
+
+            <p>The source code is available <a href="https://github.com/rockhopper72/VideoMirror">on GitHub</a></p>
+
+            <p><strong>Keywords:</strong> Browser extension, Javascript</p>
+
+            <p><strong>See it live:</strong> <a href="https://chrome.google.com/webstore/detail/videomirror/lgknkdljklhkafjfekjjbldfcdnmdpkh">VideoMirror Chrome Extension</a></p>
+        </div>
+
+        <div class="mb-4">
+            <h2>Captures by Cayla</h2>
+
+            <p>Captures by Cayla is a site I designed for a friend who needed an online portfolio of her photography. My goal for the site was to make it quick to load, even with many images. The images are optimized for web and the website makes use of a custom tabular interface for choosing categories.</p>
+
+            <p>The source code is available <a href="https://github.com/rockhopper72/capturesbycayla.com">on GitHub</a></p>
+
+            <p><strong>Keywords:</strong> HTML, CSS, Javascript</p>
+
+            <p><strong>See it live:</strong> <a href="https://rockhopper72.github.io/capturesbycayla.com/">Captures by Cayla</a></p>
+        </div>
 
         <p>&nbsp;</p>
 
-        <p>For now, check out <a href="https://github.com/rockhopper72">my GitHub</a>.</p>
+        <p>For more projects, check out <a href="https://github.com/rockhopper72">my GitHub</a>.</p>
     </div>
 </template>
