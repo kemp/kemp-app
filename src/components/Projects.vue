031bb2f4c875003e1c476eda67419c77928195f1
rockhopper72 <github2@steven.globaloffset.com>
2019-07-15 13:39:09 -0400

    Add icons to project page

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -5,11 +5,11 @@
 
             <p class="pb-2">{{ project.description }}</p>
 
-            <p class="pb-2" v-if="project.sourceUrl">The source code is available <a :href="project.sourceUrl">on GitHub</a>.</p>
+            <p class="pb-2" v-if="project.sourceUrl">The source code is available <a :href="project.sourceUrl">on GitHub <img src="/icons/github.svg" class="h-4 rounded-full" alt=""></a>.</p>
 
             <p class="pb-2" v-if="project.keywords"><strong>Keywords:</strong> <span v-for="(keyword, index) in project.keywords"><span v-if="index != 0">, </span>{{ keyword }}</span></p>
 
-            <p class="pb-2" v-if="project.liveUrl"><strong>See it live:</strong> <a :href="project.liveUrl">{{ project.name }}</a></p>
+            <p class="pb-2" v-if="project.liveUrl"><strong>See it live:</strong> <a :href="project.liveUrl" target="_blank">{{ project.name }}<img src="/icons/open_in_new.svg" class="h-4" alt=""></a></p>
         </div>
         
         <p>For more projects, check out <a href="https://github.com/rockhopper72">my GitHub</a>.</p>
