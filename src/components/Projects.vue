4f05f80fb1ced47321053bb0b17d48e798605a15
rockhopper72 <github2@steven.globaloffset.com>
2019-07-18 09:54:41 -0400

    Add option to filter projects by keyword

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-        <div class="mb-6" v-for="project in projects">
+        <div class="mb-4">
+            <p>Filter by keywords: 
+                <span v-for="keyword in allKeywords"><button @click="toggleFilter(keyword)" class="py-1 px-2 m-1 border border-grey rounded-full shadow" :class="{ 'bg-primary-dark text-primary-light': filteredKeywords.indexOf(keyword) >= 0 , 'hover:bg-grey-lighter': filteredKeywords.indexOf(keyword) == -1}">{{ keyword }}</button></span>
+                <span v-if="filteredKeywords.length"><button class="py-1 px-2 m-1 border border-grey rounded-full shadow" @click="clearFilters">&times; Clear filters</button></span>
+            </p>
+        </div>
+
+        <div class="mb-6" v-for="project in filteredProjects">
             <h2 class="font-cursive pb-2">{{ project.name }}</h2>
 
             <p class="pb-2">{{ project.description }}</p>
@@ -21,8 +28,36 @@
 
         },
 
+        methods: {
+            toggleFilter: function (keyword) {
+                if (this.filteredKeywords.indexOf(keyword) == -1) {
+                    this.filteredKeywords.push(keyword);
+                } else {
+                    this.filteredKeywords.splice(this.filteredKeywords.indexOf(keyword), 1);
+                }
+            },
+            clearFilters: function () {
+                this.filteredKeywords = [];
+            },
+            intersect: function (a, b) {
+                return a.filter(value => b.includes(value));
+            },
+        },
+
+        computed: {
+            allKeywords: function() {
+                return [...new Set(this.projects.flatMap((project) => project.keywords).sort())];
+            },
+            filteredProjects: function() {
+                if (this.filteredKeywords.length == 0) return this.projects;
+
+                return this.projects.filter(project => this.intersect(project.keywords, this.filteredKeywords).length > 0);
+            }
+        },
+
         data() {
             return {
+                filteredKeywords: [],
                 projects: [
                     { name: 'Living in Him Alone', description: 'This blog is a simple example of my expertise with frontend languages such as HTML, CSS, and Javascript (including Vue.js), as well as backend languages such as PHP and Laravel. This website is deployed to the cloud on DigitalOcean, so basic command line experience was necessary to provision the server.', keywords: ['PHP', 'Laravel', 'Javascript', 'Vue.js'], liveUrl: 'https://livinginhimalone.com' },
                     { name: 'Notarizer', description: 'Notarizer was built with PHP and the Laravel framework. I designed all pages myself using the Sketch Mac app. It has a near-complete test suite and it interacts with Stripe for payments. I’m currently in the process of making a public API for it.', keywords: ['PHP', 'Laravel', 'SVG'], liveUrl: 'https://notarizer.app', sourceUrl: 'https://github.com/notarizer/notarizer' },
